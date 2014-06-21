8443e96cc12975c6f7169d6d09d45c4b8c09389b
carlynorama <carlyn.maw@gmail.com>
2014-06-21 15:57:10 -0700

    refactored new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -45,16 +45,6 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
-    
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
         
 class ItemModelTest(TestCase): #this is the long way
 
@@ -77,12 +67,17 @@ class ItemModelTest(TestCase): #this is the long way
         
 class ListViewTest(TestCase):
 
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/lists/the-only-list-in-the-world/') #1
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertContains(response, 'itemey 1') #2
-        self.assertContains(response, 'itemey 2') #3
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+    
         
