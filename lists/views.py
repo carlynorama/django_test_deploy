c9cf95c1695735c5ae7e3557a121330fd278dbc4
carlynorama <carlyn.maw@gmail.com>
2014-06-21 14:16:35 -0700

    Dont save blank items for every request

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from lists.models import Item
 
-def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
+def home_page(request):    
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']  #1
+        Item.objects.create(text=new_item_text)  #2
+    else:
+        new_item_text = ''  #3
 
     return render(request, 'home.html', {
-        'new_item_text': item.text
+        'new_item_text': new_item_text,  #4
     })
