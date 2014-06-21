028c469c95834467dcca1333b2595f7083596f06
carlynorama <carlyn.maw@gmail.com>
2014-06-21 14:09:24 -0700

    saving post to database test 1

--- lists/tests.py
+++ lists/tests.py
@@ -26,6 +26,10 @@ class HomePageTest(TestCase):
     
         response = home_page(request)
     
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+    
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
@@ -51,3 +55,4 @@ class ItemModelTest(TestCase): #verbose style
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+        
